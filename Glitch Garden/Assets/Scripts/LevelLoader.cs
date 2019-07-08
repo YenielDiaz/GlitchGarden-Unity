e42426fd9d410fe389285df666ed6a08c4e94927
Yeniel Diaz <yenie@LAPTOP-KARDRO8G>
2019-07-08 14:22:43 -0400

    SplashScreen
    Added Splash Screen, Start Screen, and worked on Aspect Ratio

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int loadDelay = 3;
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //we can use this when going through scenes in order
+        StartCoroutine(LoadAfterDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoadAfterDelay()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        SceneManager.LoadScene("StartScreen");
+    }
+}
