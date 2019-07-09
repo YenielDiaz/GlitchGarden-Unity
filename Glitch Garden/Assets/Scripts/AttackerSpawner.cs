aac97352cbcbbb8bc3361a8274f43485df7d16ca
Yeniel Diaz <yenie@LAPTOP-KARDRO8G>
2019-07-09 19:32:39 -0400

    Spawners and Trophy
    Added Enemy Spawners for each row in the tile map, added trophy object

---
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] bool spawn = true;
+    [SerializeField] Attacker lizardPrefab;
+    [SerializeField] float minDelay = 1;
+    [SerializeField] float maxDelay = 5;
+
+    IEnumerator Start()
+    {
+        do
+        {          
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            SpawnAttacker();
+        }
+        while (spawn);
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(lizardPrefab, transform.position, transform.rotation);
+    }
+    void Update()
+    {
+        
+    }
+}
